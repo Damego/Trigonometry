db06c240e2d52294f23ab988ae15f2473002d30b
Damego <danyabatueff@gmail.com>
2020-09-28 20:44:07 +0500

    PreRelease Update 2

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class Main(ttk.Frame):
                                  width=20, height=2,
                                  bg = "#246de8", fg="white",
                                  font="Arial 16",
-                                 command=self.examMenu)
+                                 command=self.SecondStartMenu)
       self.startExamModeBtn.place(x = 150, y = 150)
 
 
@@ -301,28 +301,36 @@ class Main(ttk.Frame):
       self.wrongQuantityExercises = 0
       self.addSuccesfulExercice = 0
 
-      self.start1.place(x = 150, y = 50)
-      self.start2.place(x = 150, y = 150)
-      self.start3.place(x = 200, y = 250)
-      self.start4.place(x = 200, y = 350)
+      self.startMenu()
 
 
-# Second Menu
-   def examMenu(self):
+   # Second Menu
+   def SecondStartMenu(self):
       self.forgetStartMenu()
-
       style = ttk.Style(root)
-      style.configure("TButton", width = 10, font = "Arial 12")
+      style.configure("TButton", width = 18,padding = 15,font = "Arial 26")
+      self.StartButton_1_2 = ttk.Button(root, text = "Координата - число", style = "TButton", command =lambda:  self.preSecondMenu(1))
+      self.StartButton_1_2.place(x = 85, y = 150)
+      self.StartButton_2_1 = ttk.Button(root, text = "Число - координата", style = "TButton", command =lambda:  self.preSecondMenu(2))
+      self.StartButton_2_1.place(x = 85, y = 250)
+
+   def preSecondMenu(self, gameType):
+      self.StartButton_1_2.place_forget()
+      self.StartButton_2_1.place_forget()
+      self.StartButton = ttk.Button(root, text = "Начать", style = "TButton", command =lambda:  self.SecondMenu(gameType))
+      self.StartButton.place(x = 85, y = 150)
+
+
+   def SecondMenu(self, gameType):
+      self.StartButton.place_forget()
+      style = ttk.Style(root)
+      style.configure("TButton", width = 12, font = "Arial 12")
       style.configure("TCombobox", font = "Arial 12")
 
+      self.combobox = ttk.Combobox(root, width=15, height = 20, font = "Arial 12", style = "TCombobox")
       
 
-
-      self.combobox = ttk.Combobox(root, width=15, height = 20, font = "Arial 12", style = "TCombobox")
-      self.combobox['values'] = self.positiveNums
-      self.combobox.place(x = 100, y = 100)
-
-      self.checkButton = ttk.Button(root, text = "Проверить", command = self.check2)
+      self.checkButton = ttk.Button(root, text = "Проверить", command =lambda: self.check2(gameType))
       self.checkButton.place(x = 300, y = 100)
 
       self.JustLabel = ttk.Label(root, text = "", font = "Arial 12")
@@ -336,61 +344,59 @@ class Main(ttk.Frame):
       self.successExercisesLabel.place(x = 10, y = 275)
       self.successQuantityExercises = 0
 
-      self.wrongExercisesLabel = ttk.Label(root, text = "Ошибок:", font = "Arial 12")
+      self.wrongExercisesLabel = ttk.Label(root, text = "Неправильных:", font = "Arial 12")
       self.wrongExercisesLabel.place(x = 10, y = 300)
       self.wrongQuantityExercises = 0
 
-      self.button = ttk.Button(root, text = "Новое число ", style = "TButton", command = self.newNum2)
-      self.button.place(x = 350, y = 400)
+      self.buttonBack = tk.Button(root, text = "<", font = "Arial 16", command = self.pressedBackButtonSecondMenu)
+      self.buttonBack.place(relx = 0.01, rely = 0.01)
+      self.newNum2(gameType)
 
 
-   def newNum2(self):
+   def newNum2(self, gameType):
       self.answerID = random.randint(0,15)
-      self.list = self.coordsNums
-      self.question = self.list[self.answerID]
-      self.compAnswer = self.positiveNums[self.answerID]
-      self.JustLabel['text'] = self.question
-      self.quantityExercises += 1
-      self.allExercisesLabel['text'] = f"Всего примеров: {self.quantityExercises}"
+      
+      if gameType == 1:
+         self.combobox['values'] = self.positiveNums
+         self.combobox.place(x = 100, y = 100)
+         self.list = self.coordsNums
+         self.question = self.list[self.answerID]
+         self.compAnswer = self.positiveNums[self.answerID]
+         self.JustLabel['text'] = self.question
+      elif gameType == 2:
+         self.combobox['values'] = self.coordsNums
+         self.combobox.place(x = 100, y = 100)
+         self.list = self.positiveNums 
+         self.question = self.list[self.answerID]
+         self.compAnswer = self.coordsNums[self.answerID]
+         self.JustLabel['text'] = self.question
       
 
-   def check2(self):
+   def check2(self, gameType):
       self.userAnswer = self.combobox.get()
       if self.compAnswer == self.userAnswer:
          self.successQuantityExercises += 1
+         self.quantityExercises += 1
+         self.allExercisesLabel['text'] = f"Всего примеров: {self.quantityExercises}"
          self.successExercisesLabel['text'] = f"Правильных: {self.successQuantityExercises}"
-         self.newNum2()
+         self.newNum2(gameType)
       else:
          self.wrongQuantityExercises += 1
-         self.wrongExercisesLabel['text'] = f"Ошибок: {self.wrongQuantityExercises}"
+         self.quantityExercises += 1
+         self.allExercisesLabel['text'] = f"Всего примеров: {self.quantityExercises}"
+         self.wrongExercisesLabel['text'] = f"Неправильных: {self.wrongQuantityExercises}"
+         self.newNum2(gameType)
 
 
-
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   def pressedBackButtonSecondMenu(self):
+      self.buttonBack.place_forget()
+      self.JustLabel.place_forget()
+      self.successExercisesLabel.place_forget()
+      self.allExercisesLabel.place_forget()
+      self.wrongExercisesLabel.place_forget()
+      self.combobox.place_forget()
+      self.checkButton.place_forget()
+      self.startMenu()
 
 
 
